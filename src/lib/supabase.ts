638b2a2a399c435fd5abc5dc083c42c274d4bfeb
ordioflow026-commits <ordioflow026@gmail.com>
2026-04-21 22:28:22 +0100

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xxyfklfyzegnuyvncyqt.supabase.co/rest/v1/';
+const supabaseUrl = 'https://xxyfklfyzegnuyvncyqt.supabase.co';
 const supabaseKey = 'sb_publishable_LvGmrUhoevb83MXKV5WgPA_76FPlA4B';
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
